3aca9ce272779c6c996e80f343c2b7286311e659
Eugene Gorelik <gene+1@pdffiller.com>
2019-10-21 18:39:35 -0400

    Added Webhook aexample

--- examples/17_download_signed_document.py
+++ examples/17_download_signed_document.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     file_name = "signed_document"
 
     # Download signed document
-    signnow_python_sdk.Document.download(access_token['access_token'], document_id, dir_path,file_name, enclose_document_history)
+    signnow_python_sdk.Document.download(access_token['access_token'], document_id, file_name, dir_path, enclose_document_history)
